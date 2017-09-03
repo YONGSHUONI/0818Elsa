6faefe4762cda227f8192a81a8fe3e569293a84d
Elsa Chen <31591307+elsa0818@users.noreply.github.com>
2017-09-03 15:30:37 +0100

    Add files via upload

---
+++ src/main/java/Elsa0818/Elsa/App.java
@@ -0,0 +1,13 @@
+package Elsa0818.Elsa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
